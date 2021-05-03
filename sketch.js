93878c317a0ea38bd19dca3c0497e5bc398b06e9
yintze <83564949+yintze@users.noreply.github.com>
2021-05-03 11:59:07 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
+let imageModelURL = ' https://yintze.github.io/bread-/';
 
 // Video
 let video;
@@ -26,10 +26,33 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(320, 260);
+  createCanvas(320, 320);
   // Create the video
-  video = createCapture(VIDEO);
-  video.size(320, 240);
+  
+   var constraints = {
+
+    audio: false,
+
+    video: {
+
+      facingMode: {
+
+        exact: "environment"
+
+      }
+
+    }   
+
+    //video: {
+
+      //facingMode: "user"
+
+    //}
+
+  }; 
+ 
+  video = createCapture(constraints);
+  video.size(320, 320);
   video.hide();
 
   flippedVideo = ml5.flipImage(video)
@@ -65,6 +88,7 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+  ThunkableWebviewerExtension.postMessage(label);
   // Classifiy again!
   classifyVideo();
 }
